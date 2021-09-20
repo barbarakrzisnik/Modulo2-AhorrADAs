976b01993dc4696fe4bd29b2b79aeb712ba90708
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2021-09-20 09:17:48 -0300

    funcionamiento botones (revisar)

---
+++ index.js
@@ -0,0 +1,27 @@
+const botonBalance = document.getElementById("boton-balance")
+const botonCategorias = document.getElementById("boton-categorias")
+const botonReportes = document.getElementById("boton-reportes")
+
+const sectionBalance = document.getElementById("section-balance")
+const sectionCategorias = document.getElementById("section-categorias")
+const sectionReportes = document.getElementById("section-reportes")
+
+
+
+botonBalance.onclick = () => {
+    sectionBalance.classList.remove("is-hidden")
+    sectionCategorias.classList.add("is-hidden")
+    sectionReportes.classList.add("is-hidden")
+}
+
+botonCategorias.onclick = () => {
+    sectionBalance.classList.add("is-hidden")
+    sectionCategorias.classList.remove("is-hidden")
+    sectionReportes.classList.add("is-hidden")
+}
+
+botonReportes.onclick = () => {
+    sectionBalance.classList.add("is-hidden")
+    sectionCategorias.classList.add("is-hidden")
+    sectionReportes.classList.remove("is-hidden")
+}
